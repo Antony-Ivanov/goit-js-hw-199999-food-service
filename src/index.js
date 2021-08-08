405fd01bb6466543ce60766a536e5c860d7cde14
Ihor Rudniev <rudnievihor@gmail.com>
2021-08-08 22:14:47 +0300

    add-files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import './sass/main.scss';
-import './templates/card-templates.hbs';
+import data from './menu.json';
+import template from './template.hbs';
+
+// console.log(data);
+// console.log(template);
+
+var result = data.map(item => template(item)).join('');
+// console.log(result);
+
+document.querySelector('.js-menu').innerHTML = result;
